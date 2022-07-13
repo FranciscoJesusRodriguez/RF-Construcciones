af2fb1ee112b9e25dabe9d80ec7211a726acbcb1
Francisco Rodriguez <coto.fjr@gmail.com>
2022-07-12 22:55:11 -0300

    Responsive MOBILE con menú

---
+++ scripts.js
@@ -0,0 +1,14 @@
+jQuery('document').ready(function($){
+    var menuBtb = $('.menu-icono'),
+        menu = $('.navegacion ul');
+
+    menuBtb.click(function() {
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }
+            else{
+                menu.addClass('show');
+            }
+        
+    });
+});
